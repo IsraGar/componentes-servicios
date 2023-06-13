f3c3f8b20d2fa26df99245728baf9e2876a91edc
Israel Garcia <isragarciahdez@gmail.com>
2023-06-13 11:44:25 -0600

    Problema de CORS y agregando archivo proxy

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { CretateProductDTO, Product, UpdateProductDTO } from '../models/product.model';
+import { retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  private apiUrl = 'https://young-sands-07814.herokuapp.com/api/products';
+  private apiUrl = '/api/products';
 
   constructor(private http: HttpClient) { }
   
@@ -17,7 +18,10 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', offset);
     }
-    return this.http.get<Product[]>(this.apiUrl, { params });
+    return this.http.get<Product[]>(this.apiUrl, { params })
+    .pipe(
+      retry(3)
+    );
   }
 
   getProduct(id: string){
@@ -41,4 +45,5 @@ export class ProductsService {
   deleteProduct(id: string){
     return this.http.delete<boolean>(this.apiUrl + '/' + id);
   }
+
 }
